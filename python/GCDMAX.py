8bd07d4172a497b9eeb5a705583b89e05185bf1f
thanhnho417 <trieudoquoc417@outlook.com>
2024-05-13 14:54:30 +0700

    add from school

--- python/GCDMAX.py
+++ python/GCDMAX.py
@@ -1,10 +1,7 @@
-from thuvien import ucln,bcnn
+from thuvien import max_gcd
 t = int(input("Nhập số nguyên dương t: "))
 A = [int(k) for k in input("Nhập " + str(t) + "số cách nhau bởi dấu cách: " ).split()]
 B = []
-for i in range(t):
-    for j in range(len(A)-1):
-        while i < j:
-            if ucln(i,A[j]) == ucln(i+1,A[j+1]):
-                B.append(i)
+for i in range(len(A)):
+    B.append(max_gcd(A[i]))
 print(B)
--- python/thuvien.py
+++ python/thuvien.py
@@ -1,7 +1,14 @@
-def ucln(a,b):
+def gcd(a,b):
     if b == 0:
         return a
-    return ucln(b,a%b)
+    return gcd(b,a%b)
 
-def bcnn(a,b):
-    return a*b//ucln(a,b)
+def lcm(a,b):
+    return a*b//gcd(a,b)
+
+def max_gcd(n):
+    max_gcd_value = 0
+    for a in range(1, n):
+        for b in range(a+1, n+1):
+            max_gcd_value = max(max_gcd_value, gcd(a, b))
+    return max_gcd_value
