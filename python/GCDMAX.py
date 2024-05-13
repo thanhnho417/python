fb477d37fec893ee30ddbd1accb4c850404ba1b2
thanhnho417 <trieudoquoc417@outlook.com>
2024-05-13 14:56:50 +0700

    a

--- python/GCDMAX.py
+++ python/GCDMAX.py
@@ -1,7 +1,8 @@
 from thuvien import max_gcd
 t = int(input("Nhập số nguyên dương t: "))
-A = [int(k) for k in input("Nhập " + str(t) + "số cách nhau bởi dấu cách: " ).split()]
+A = [int(k) for k in input("Nhập" + " " + str(t) + " " + "số cách nhau bởi dấu cách: " ).split()]
 B = []
 for i in range(len(A)):
     B.append(max_gcd(A[i]))
-print(B)
+for i in B:
+    print(i)
--- python/thuvien.py
+++ python/thuvien.py
@@ -7,8 +7,9 @@ def lcm(a,b):
     return a*b//gcd(a,b)
 
 def max_gcd(n):
-    max_gcd_value = 0
+    t = 0
     for a in range(1, n):
         for b in range(a+1, n+1):
-            max_gcd_value = max(max_gcd_value, gcd(a, b))
-    return max_gcd_value
+            t = max(t, gcd(a, b))
+    return t
+
