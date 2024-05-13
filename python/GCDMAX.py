9bbaf2d3467174d438314e5f124beb9d2a55c498
thanhnho417 <trieudoquoc417@outlook.com>
2024-05-13 14:46:08 +0700

    add from school

--- python/GCDMAX.py
+++ python/GCDMAX.py
@@ -1,7 +1,10 @@
-def ucln(a,b):
-    if b == 0:
-        return A
-    return ucln(b,a%b)
-
+from thuvien import ucln,bcnn
 t = int(input("Nhập số nguyên dương t: "))
-A = [int(k) for k in input("Nhập " + str(t) + "số cách nhau bởi dấu cách: " )]
+A = [int(k) for k in input("Nhập " + str(t) + "số cách nhau bởi dấu cách: " ).split()]
+B = []
+for i in range(t):
+    for j in range(len(A)-1):
+        while i < j:
+            if ucln(i,A[j]) == ucln(i+1,A[j+1]):
+                B.append(i)
+print(B)
