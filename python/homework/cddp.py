742ece7ae43e8648536bfbbfaf960a331e76b2f5
thanhnho417 <trieudoquoc417@outlook.com>
2024-06-09 22:18:54 +0700

    add from homework

--- python/homework/cddp.py
+++ python/homework/cddp.py
@@ -2,7 +2,10 @@
 n = int(input("Nhập số nguyên dương n: "))
 A = list(map(int,input("Nhập dãy A gồm " + str(n) + " số: ").split()))
 t = 0
-for i in range(1,len(A)-1):
-    if A[i] > A[i-1] and A[i] > A[i+1]:
-        t = t + 1
-print(t)
+if n < 3:
+    print("0")
+else: 
+    for i in range(1,len(A)-1):
+        if A[i] > A[i-1] and A[i] > A[i+1]:
+            t = t + 1
+    print(t)
