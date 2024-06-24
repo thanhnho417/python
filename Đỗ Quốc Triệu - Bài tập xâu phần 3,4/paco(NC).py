f1be3ac5b7c1eee7e7a9aa7f816eb47ef766b1d0
thanhnho417 <trieudoquoc417@outlook.com>
2024-06-24 17:07:26 +0700

    update

--- Đỗ Quốc Triệu - Bài tập xâu phần 3,4/paco(NC).py
+++
@@ -1,7 +0,0 @@
-n,k = map(int,input().split())
-s = reversed(input())
-t = 0
-for i in s:
-    if i == i+1:
-        ans = ans + 1
-        
--- Đỗ Quốc Triệu - Bài tập xâu phần 3,4/twoc.py
+++ Đỗ Quốc Triệu - Bài tập xâu phần 3,4/twoc.py
@@ -2,9 +2,16 @@ n = int(input())
 s = input().strip()
 ans = ""
 def kt(s):
+    temp = 0
+    ans = ""
     for i in s:
-        if i*2 in s:
+        if i not in ans:
+            temp += 1
+            ans+= i
+    for i in s:
+        if i*2 in s or temp == 3:
             return False
+            break
     return True
 for i in s:
     k = s.replace(i,"")
